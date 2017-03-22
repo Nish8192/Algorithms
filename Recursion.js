a744b47f28408e94e48b1f9cadcb769d6e50201e
Nishant Aggarwal <nish8192@gmail.com>
2017-03-22 10:56:13 -0400

    Recursion work

--- Recursion.js
+++ Recursion.js
@@ -38,3 +38,69 @@ function rFibonacci(a){
     return rFibonacci(a - 1) + rFibonacci(a - 2);
 }
 // console.log(rFibonacci(10));
+
+    //Recursively find if a value exists in arr w/ Binary Search
+function rBinarySearch(arr, val){
+    if(arr.length <= 1){
+        if(arr[0] === val){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    var min = 0, max = arr.length - 1;
+    var i = Math.floor((max + min) / 2);
+    if(arr[i] === val){
+        return true;
+    }
+    if(arr[i] > val){
+        return rBinarySearch(arr.slice(0, i), val);
+    }
+    if(arr[i] < val){
+        return rBinarySearch(arr.slice(i + 1, max + 1), val);
+    }
+}
+// console.log(rBinarySearch([1, 3, 5, 6], 4));
+// console.log(rBinarySearch([4, 5, 6, 8, 12], 5));
+
+    //Recursively replace all '?' with either 1 or 0 returning an array of all possible outcomes
+function rStringExpansion(str){
+
+}
+
+function rFloodFill(canvas, sXY, newColor){
+    var currColor = canvas[sXY[0]][sXY[1]];
+    canvas[sXY[0]][sXY[1]] = newColor;
+    if(sXY[0] > 0){
+        if(canvas[sXY[0] - 1][sXY[1]] === currColor){
+            sXY[0]--;
+            return rFloodFill(canvas, sXY, newColor);
+        }
+    }
+    if(sXY[1] > 0){
+        if(canvas[sXY[0]][sXY[1] - 1] === currColor){
+            sXY[1]--;
+            return rFloodFill(canvas, sXY, newColor);
+        }
+    }
+    if(sXY[0] < canvas.length - 1){
+        if(canvas[sXY[0] + 1][sXY[1]] === currColor){
+            sXY[0]++;
+            return rFloodFill(canvas, sXY, newColor);
+        }
+    }
+    if(sXY[1] < canvas[sXY[0]].length - 1){
+        if(canvas[sXY[0]][sXY[1] + 1] === currColor){
+            sXY[1]++;
+            return rFloodFill(canvas, sXY, newColor);
+        }
+    }
+    return canvas;
+}
+var canvas = [ [3,2,3,4,3],
+                [2,3,3,4,0],
+                [7,3,3,5,3],
+                [6,5,3,4,1],
+                [1,2,3,3,3]]
+    console.log(rFloodFill(canvas, [2,2], 1));
